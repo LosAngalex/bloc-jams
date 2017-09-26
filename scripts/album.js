060ebb0504b6275970981b08938288dfca29c5e1
Alex <losangalex@gmail.com>
2017-09-26 02:45:41 -0700

    DOM Scripting Play Pause Pt 2

--- scripts/album.js
+++ scripts/album.js
@@ -82,12 +82,36 @@
      }
  };
 
+var clickHandler = function(targetElement) {
+    
+    var songItem = getSongItem(targetElement);  
+    
+    if (currentlyPlayingSong === null) {
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+        
+    } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+    songItem.innerHTML = playButtonTemplate;
+    currentlyPlayingSong = null;
+    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+    
+ };
+
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 
 // Album button templates
- var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+// Store state of playing songs
+ var currentlyPlayingSong = null;
 
 
  window.onload = function() {
@@ -96,17 +120,22 @@ var songRows = document.getElementsByClassName('album-view-song-item');
      
      songListContainer.addEventListener('mouseover', function(event) {
          if (event.target.parentElement.className === 'album-view-song-item') {
-             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+
+event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
          
-         });
+    });
          
      
-    for (var i = 0; i < songRows.length; i++) {
-        songRows[i].addEventListener('mouseleave', function(event) {
+for (var i = 0; i < songRows.length; i++) {
+    songRows[i].addEventListener('mouseleave', function(event) {
         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
          });
-     }
+
+songRows[i].addEventListener('click', function(event) {
+    clickHandler(event.target);
+ });     
+}
 
 
      var albums = [albumPicasso, albumMarconi, albumAlex];
